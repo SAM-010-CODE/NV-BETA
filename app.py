66b6e90f8051da2736eb5538320c12ccc16d586b
SAM-010-CODE <kennylextra@gmail.com>
2025-03-25 17:17:25 +0100

    Fix NameError by defining app variable in FastAPI instance

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
